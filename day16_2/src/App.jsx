5503f728325288e882eba5dde0bc3f3c9c7c0812
viren76 <84906860+viren76@users.noreply.github.com>
2021-07-14 20:21:27 +0530

    Add Day16_2

---
+++ day16_2/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./components/Card";
+
+function App(){
+    return(
+        <div className="App">
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+            <Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+        </div>
+    )
+}
+export default App;
